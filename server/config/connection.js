3726a48a081c4a28996bbc0aff34b1ba729cdbda
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-14 16:56:25 -0600

    renamed db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/the-book-store', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/the-book-store-2', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
