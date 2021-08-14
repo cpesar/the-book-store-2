f5a889d83b3ac0ca0b676707cd881b66d6effb0e
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-14 16:54:35 -0600

    troublehooting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/the-book-store-2', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/the-book-store', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
